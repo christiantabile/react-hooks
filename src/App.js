09e8ddb95b56d0bd19e44a43ea6942f03590669e
catmac <christiantabile@gmail.com>
2022-03-08 21:05:20 -0800

    Implement example of using useState hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+
+// Can only use hooks on function components, must be top level 'hook()'
+// Cannot be nested in anything (if's, functions, loops, etc.)
 function App() {
+
+  const [count, setCount] = useState(() => {
+    console.log('run function')
+    return 4
+  }) // Pass in default state, useState returns two values [currentState, updateState]
+
+  function decrementCount() {
+    setCount(prevCount => prevCount - 1)
+  }
+
+  function incrementCount() {
+    setCount(prevCount => prevCount + 1)
+  }
+
   return (
     <>
-      <button>-</button>
-      <span>0</span>
-      <button>+</button>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
     </>
   )
 }
